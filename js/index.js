e858aa440ac2e233e8e51a6c9c400c5a4eaa6280
ganesh patil <patilganu873@gmail.com>
2024-10-05 15:59:29 +0530

    updates

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
-function asynFunction()
-{
-    return new Promise( (resolve,reject)=>{
-        setTimeout( ()=>{
-            console.log('UserName Matched nice');
-            resolve('pass');
-        },4000)
-    });
-}
+const password = document.querySelector('#password');
 
-let promise = asynFunction();
+const btn = document.getElementById('btn');
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()";
+const allChars = upperCase + lowerCase + number + symbol;
 
-// promise.then( (resolve)=>{
-//     console.log(resolve)
-// })
+let randomPass = ()=>{
+    let password = "";
+    password += upperCase[Math.floor(Math.random() *upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() *lowerCase.length)];
+    password += number[Math.floor(Math.random() *number.length)];
+    password += symbol[Math.floor(Math.random() *symbol.length)];
 
+    while(length > password.length)
+    {
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+    }
+    this.password.value = password;
+};
+// let copyPassWord = ()=>
+//     {
+//        password.select();
+//        document.execCommand('copy');
+    
+//     }
+let copyPassWord = ()=>{
+    let value = password.value;
+    navigator.clipboard.writeText(value);
+};
+
+btn.addEventListener('click', randomPass);
+document.querySelector('img').addEventListener('click' , copyPassWord);
 
